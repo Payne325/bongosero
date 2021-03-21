c24d55a1ae77575c8af99d498210a972ffc17244
Payne325 <alex.payne325@gmail.com>
2021-03-21 13:11:43 +0000

    All weapon related polling removed from bongosero_movement

--- src/bongosero_movement.rs
+++ src/bongosero_movement.rs
@@ -9,39 +9,50 @@ type Boundingbox = ((i32, i32), (i32, i32));
 
 // Todo: these properties can live somewhere in a singleton/global space.
 // Maybe they can be stored in a settings file? JSON?
-
 const MOVE_DEAD_ZONE: f32 = 3.0;
 const SCREEN : (i32, i32) = (800, 600); //Game screen coord space
 const F_TRAK_MAX_BNDS : (i32, i32) = (600, 420); //f-trak coord space -> Todo. Create some sort of calibration routine to get this
 
 pub struct BongoseroMovement {
-   m_a: bool,
-   m_firing: bool,
    m_terminated : bool,
    m_bbox_receiver : mpsc::Receiver<Boundingbox>,
    m_terminate_receiver : mpsc::Receiver<bool>,
-   m_prev_position : f32,
-   m_velocity_history : Vec<f32>,
-   m_speed: f32,
-   m_begun: bool
+   m_prev_position : f32
 }
 
-fn f_trak_to_screen_coords(position: f32) -> f32 {
-   
-   let edge_of_screen_offset = 20; //Handle literal edge case
-   let min = 0.0 + edge_of_screen_offset as f32; 
-   let max = (F_TRAK_MAX_BNDS.0 - edge_of_screen_offset) as f32;
-   let norm_pos = (position - min) / (max - min);
+impl BongoseroMovement {
+   fn f_trak_to_screen_coords(&self, position: f32) -> f32 {
 
-   //multiply to move to new coord system
-   norm_pos * SCREEN.0 as f32
+      let edge_of_screen_offset = 20; //Handle literal edge case
+      let min = 0.0 + edge_of_screen_offset as f32; 
+      let max = (F_TRAK_MAX_BNDS.0 - edge_of_screen_offset) as f32;
+      let norm_pos = (position - min) / (max - min);
+
+      //multiply to move to new coord system
+      norm_pos * SCREEN.0 as f32
+   }
+
+   fn get_current_position(&self) -> f32 {
+      
+      let mut new_pos = -1.0;
+      let current_bbox = self.m_bbox_receiver.try_recv();
+
+      match current_bbox {
+         Ok(t) => {
+            new_pos = self.f_trak_to_screen_coords((t.1.0 as f32 + t.0.0 as f32) / 2.0);
+         },
+         Err(_) => { /*println!("ERROR: {}", e);*/ },
+      }
+
+      new_pos
+   }
 }
 
-// Controller used for debuging the game without the bongo controller, keyboard
+// Polls f-trak for player face location and converts to game coords
 impl input_device::InputDevice for BongoseroMovement {
    fn new() -> Self {
 
-      //Todo: relative path
+      //Todo: relative path -> JSON File?
       let protopath = "D:/Portfolio/f-trak/f-trak/static/deploy.prototxt.txt".to_string();
       let modelpath = "D:/Portfolio/f-trak/f-trak/static/model.caffemodel".to_string();
       let min_confidence = 0.9;
@@ -59,91 +70,28 @@ impl input_device::InputDevice for BongoseroMovement {
          face_cap.begin_capture();
       });
 
-      //Get intial position from f-trak
-      let initial_position : f32;
-
-      loop {
-         let val = bbox_receiver.try_recv();
-
-         match val {
-            Ok(t) => { 
-               initial_position = f_trak_to_screen_coords((t.1.0 as f32 + t.0.0 as f32) / 2.0);
-              break;
-            },
-            Err(_) => { /*println!("ERROR: {}", e);*/ },
-         }
-      }
-
       BongoseroMovement {
-         m_a: false,
-         m_firing: false,
          m_terminated: false, 
          m_bbox_receiver: bbox_receiver,
          m_terminate_receiver: terminate_receiver,
-         m_prev_position : initial_position,
-         m_velocity_history : Vec::with_capacity(8),
-         m_speed: 200.0,
-         m_begun: false
+         m_prev_position : -1.0,
       }
    }
    
-   fn poll(&mut self, input: &qs::Input) -> input_device::UserCommand {
-           
-      if !self.m_begun {
-         //Used to place player at correct position for their given face location
-         self.m_begun = true;
-         return input_device::UserCommand::new_experiment(
-            Vector::new(0.0, 0.0), 
-            Vector::new(self.m_prev_position as f32, 516.0), 
-            false)
+   fn poll(&mut self, _input: &qs::Input) -> input_device::UserCommand {    
+      //Get face location from f-trak
+      let new_pos = self.get_current_position();
+
+      //if this is the first recorded face position, 
+      //or if the movement exceeds deadzone, them record the new position
+      if self.m_prev_position == -1.0 {
+         self.m_prev_position = new_pos;
       }
 
-      self.m_a = input.key_down(qs::input::Key::Space);
-      
-      //Get the move_dir from f-trak
-      let mut move_dir = Vector::new(0.0, 0.0);
-      let current_bbox = self.m_bbox_receiver.try_recv();
-
-      match current_bbox {
-         Ok(t) => {
-            let new_pos = f_trak_to_screen_coords((t.1.0 as f32 + t.0.0 as f32) / 2.0);
-            let move_vec = new_pos - self.m_prev_position;
-
-            if self.m_velocity_history.len() == 8 {
-               self.m_velocity_history.pop();
-            }
-
-            self.m_velocity_history.push(move_vec);
-
-            let mut total_vel_history = 0.0;
-
-            for i in 0..self.m_velocity_history.len() {
-               total_vel_history += self.m_velocity_history[i];
-            }
-
-            total_vel_history = total_vel_history/self.m_velocity_history.len() as f32;
-
-            if total_vel_history.abs() > MOVE_DEAD_ZONE { 
-
-               self.m_velocity_history.clear();
-               move_dir = Vector::new(total_vel_history as f32, 0.0);
-
-               move_dir *= self.m_speed;
-
-               self.m_prev_position = new_pos;
-            }
-         },
-         Err(_) => { /*println!("ERROR: {}", e);*/ },
-      }
-
-      let shoot = |firing: bool, a_down: bool| -> bool {
-         if !firing && a_down {
-            return true
-         }        
-         false
-      }(self.m_firing, self.m_a);
-
-      self.m_firing = self.m_a;
+      if new_pos != -1.0 &&
+         (new_pos - self.m_prev_position).abs() > MOVE_DEAD_ZONE { 
+         self.m_prev_position = new_pos;      
+      }      
 
       //Handle possible f-trak termination
       let cancel = self.m_terminate_receiver.try_recv();
@@ -154,16 +102,12 @@ impl input_device::InputDevice for BongoseroMovement {
       }
 
       input_device::UserCommand::new_experiment(
-         move_dir, 
+         Vector::ZERO, 
          Vector::new(self.m_prev_position as f32, 516.0), 
          false)
    }
 
    fn debug_print(&self) {
-      if self.m_a {
-         println!("Space Button Pressed Down!");
-      }
-
       //Todo: Further f-trak related debug information
       if self.m_terminated {
          println!("f-trak thread was terminated!")
