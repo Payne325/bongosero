69b9abbc0a935840d78b0e6f60a3c4ff7dca7249
Payne325 <alex.payne325@gmail.com>
2021-08-30 16:20:06 +0100

    Enemies place randomly in x axis

--- src/enemy_factory.rs
+++ src/enemy_factory.rs
@@ -1,4 +1,4 @@
-use rand::random;
+use rand::Rng;
 use crate::phys::Phys;
 use quicksilver::geom::Vector;
 use std::collections::VecDeque;
@@ -18,12 +18,15 @@ impl EnemyFactory {
 
    pub fn tick(&mut self, phys: &mut Phys) {
       self.m_difficulty = 1.0 - (1.0 - self.m_difficulty) * 0.9999975;
-
-      if random::<f32>() < self.m_difficulty + 0.005 {
+      
+      let mut rng = rand::thread_rng();
+      
+      if rng.gen::<f32>() < self.m_difficulty + 0.005 {
          //create new enemy
+         let x = rng.gen_range(32.0, 768.0);
          let enemy = phys
             .create_body()
-            .set_pos(Vector::new(368.0, 10.0)) //x coord needs to be random
+            .set_pos(Vector::new(x, 10.0)) //x coord needs to be random
             .set_vel(Vector::new(0.0, 40.0))
             .set_radius(64.0)
             .set_mass(1.0)
